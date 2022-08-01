53e247bd0fe94defd6ccb25825ca1f2dd2f71b4b
D0N-B0T <13562845+D0N-B0T@users.noreply.github.com>
2022-08-01 18:07:09 -0400

    asd

--- modules/findGithub.py
+++ modules/findGithub.py
@@ -5,8 +5,6 @@ def getGit(username):
     try:    
         url = "https://api.github.com/users/"+username+"/events/public"
         r = requests.get(url)
-        
-        #if return code is {} then user does not exist
         displaylogin  = r.json()[0]['actor']['display_login']
         email = r.json()[0]['payload']['commits'][0]['author']['email']
 
@@ -14,11 +12,8 @@ def getGit(username):
         r2 = requests.get(url2)
         id = r2.json()['id']
         avatar = r2.json()['avatar_url']
-        #save avatar
-        with open('avatar.jpg', 'wb') as f:
-            f.write(requests.get(avatar).content)
 
-        return 'ID:'+'\n' + id +'Github nombre: '+ displaylogin +'\n'+ 'Github mail: '+ email
+        return 'ID:'+'\n' + id +'Github nombre: '+ displaylogin +'\n'+ 'Github mail: '+ email  +'\n'+ 'Github avatar: '+ avatar
         
 
     except:
