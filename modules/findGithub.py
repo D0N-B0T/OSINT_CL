5bd59d4a03360e9d30d994a1771dfda0c2424584
D0N-B0T <13562845+D0N-B0T@users.noreply.github.com>
2022-08-01 18:28:59 -0400

    asd

--- modules/findGithub.py
+++ modules/findGithub.py
@@ -5,10 +5,17 @@ def getGit(username):
     try:    
         url = "https://api.github.com/users/"+username+"/events/public"
         r = requests.get(url)
-        
         displaylogin  = r.json()[0]['actor']['display_login']
         email = r.json()[0]['payload']['commits'][0]['author']['email']
-        return 'Github nombre: '+ displaylogin +'\n'+ 'Github mail: '+ email
+
+        url2 = "https://api.github.com/users/"
+        r2 = requests.get(url2+username)
+        id = r2.get('id')
+        avatar = r2.get('avatar_url')
+        
+
+
+        return 'ID: '+'\n' + id +'Github nombre: '+ displaylogin +'\n'+ 'Github mail: '+ email  +'\n'+ 'Github avatar: '+ avatar
     except:
-        return 'Github: User details not found'
+        return 'Github: error de conexion'
     
