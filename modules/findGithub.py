50508f4c384782eff918840d00f806b6da27fe20
D0N-B0T <13562845+D0N-B0T@users.noreply.github.com>
2022-08-01 18:26:07 -0400

    asd

--- modules/findGithub.py
+++ modules/findGithub.py
@@ -8,14 +8,14 @@ def getGit(username):
         displaylogin  = r.json()[0]['actor']['display_login']
         email = r.json()[0]['payload']['commits'][0]['author']['email']
 
-        url2 = "https://api.github.com/users/"+username
-        r2 = requests.get(url2)
-        id = r2.json()['id']
-        avatar = r2.json()['avatar_url']
-
-        return 'ID:'+'\n' + id +'Github nombre: '+ displaylogin +'\n'+ 'Github mail: '+ email  +'\n'+ 'Github avatar: '+ avatar
+        url2 = "https://api.github.com/users/"
+        r2 = requests.get(url2+username)
+        id = r2.get('id')
+        avatar = r2.get('avatar_url')
         
 
+
+        return 'ID: '+'\n' + id +'Github nombre: '+ displaylogin +'\n'+ 'Github mail: '+ email  +'\n'+ 'Github avatar: '+ avatar
     except:
         return 'Github: error de conexion'
     
